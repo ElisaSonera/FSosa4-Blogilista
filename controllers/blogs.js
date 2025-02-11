de38cff4b234583c8390ec01ccd55ff319aae9ba
ElisaSonera <elisa.laatu@gmail.com>
2025-02-11 16:09:30 +0200

    Routet asyncattu

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,27 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
+//asynced
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then((blog) => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => next(error))
+//asynced
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (excpection) {
+    next(excpection)
+  }
 })
 
+//asynced
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -25,22 +29,25 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes || 0 // toimiikohan näin
   })
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch((error) => next(error))
+//asynced
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+//asynced
+blogsRouter.put('/:id', async (request, response, next) => {
   const body = request.body
 
   const blog = {
@@ -50,11 +57,8 @@ blogsRouter.put('/:id', (request, response, next) => {
     likes: body.likes
   }
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then((updatedBlog) => {
-      response.json(updatedBlog)
-    })
-    .catch((error) => next(error))
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -69,8 +69,7 @@ test('a valid blog can be added', async () => {
 //4.11 Testi, joka varmistaa, että jos kentälle likes ei anneta arvoa, sen arvoksi asetetaan 0
 // (Laajenna ohjelmaa)
 
-//4.12 Testit HTTP POST pyynnölle, jotka varmistavat, että jos uusi blogi ei sisällä kenttää title tai url, niin pyyntään vastataan statuskoodilla 400 bad request
-// (Laajenna ohjelmaa)
+//4.12 puuttuu: (Laajenna ohjelmaa)
 
 test('blog without title or url is not added', async () => {
   const newBlog = {
@@ -83,6 +82,37 @@ test('blog without title or url is not added', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+// ...
+test('a specific blog can be viewed', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+
+  const blogToView = blogsAtStart[0]
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultBlog.body, blogToView)
+})
+
+// ...
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const titles = blogsAtEnd.map(r => r.title)
+  assert(!titles.includes(blogToDelete.title))
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
 })
 
 
